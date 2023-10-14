08be89aa4e57b23239da7ef0f5e5047453a79fd6
Marian <stanmarianfb@yahoo.com>
2023-10-14 14:39:31 +0300

    Small updates

---
+++ cypress/e2e/TestareSaucedemo/Testare Login.cy.js
@@ -0,0 +1,23 @@
+describe("Login site saucedemo.com", () => {
+
+    it("Login with wrong pass and user", () => {
+        
+        cy.visit('https://www.saucedemo.com')
+        /*cy.get('[data-test="username"]').type("standard_userRRRRRRRR");
+        cy.get('[data-test="password"]').type("secret_sauce");
+        cy.get('[data-test="login-button"]').click();*/
+
+    })
+
+    /*it("Login with right pass and user", () => {
+        
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type("standard_user");
+        cy.get('[data-test="password"]').type("secret_sauce");
+        cy.get('[data-test="login-button"]').click();
+
+    })*/
+
+
+
+    })
--- cypress/e2e/TesteEroti/test1.cy.js
+++ cypress/e2e/TesteEroti/test1.cy.js
@@ -1,17 +1,45 @@
 describe("Site-ul e-roti.ro", () => {
 
-    it("Functionalitate searchbar", () => {
+    it("Login with wrong password or username", () => {
         cy.visit("https://www.e-roti.ro/");
-        cy.get("#searchBox").type("2055516 continental").type("{enter}");
-    
-
-        cy.get(".searchProdus").should("exist");
-
+        cy.get('.menu > :nth-child(2) > a').click();
+        cy.get('#email').type("red.somedayz@gmail.com");
+        cy.get('#parola').type("iwkerklje{enter}");
+        cy.get(':nth-child(4) > .text-center').should.exist;   
 
     })
 
+    it("Login with right password or username", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get('.menu > :nth-child(2) > a').click();
+        cy.get('#email').type("red.somedayz@gmail.com");
+        cy.get('#parola').type("tambur00{enter}");
+        cy.get('#siteContent > :nth-child(5)').should.exist;   
 
+    })
 
+    it("Login then logout", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get('.menu > :nth-child(2) > a').click();
+        cy.get('#email').type("red.somedayz@gmail.com");
+        cy.get('#parola').type("tambur00{enter}");
+        cy.get('#siteContent > :nth-child(5) > a').click();
+        cy.get('.text-right > .menu > :nth-child(2)').click();
+        cy.get('.cardTitle').should.exist;
 
+    })
+
+    it("Cautare produs", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get('#latimeSelect').select('185');
+        cy.get('#inaltimeSelect').select('65');
+        cy.get('#razaSelect').select('15');
+        cy.get('#brandSelect').select("DEBICA");
+        cy.get('#anotimpuriSelect').select("Vara");
+        cy.get('.bucataFiltru.col-sm-12 > #functiiSpecialeSelect > :nth-child(1) > input').check();
+        cy.get('.bucataFiltru.col-sm-12 > #functiiSpecialeSelect > :nth-child(2) > input').check();
+        cy.get('.col-lg-4 > .cautaBtn').click();
+
+    })
 } )
 
