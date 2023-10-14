e3b9280b3951c7fb8c1fb66f633791b38d385e9e
Marian <stanmarianfb@yahoo.com>
2023-10-14 15:45:00 +0300

    Finish test suite

--- cypress/e2e/TesteEroti/test1.cy.js
+++ cypress/e2e/TesteEroti/test1.cy.js
@@ -29,7 +29,7 @@ describe("Site-ul e-roti.ro", () => {
 
     })
 
-    it("Cautare produs", () => {
+    it("Search product", () => {
         cy.visit("https://www.e-roti.ro/");
         cy.get('#latimeSelect').select('185');
         cy.get('#inaltimeSelect').select('65');
@@ -38,8 +38,47 @@ describe("Site-ul e-roti.ro", () => {
         cy.get('#anotimpuriSelect').select("Vara");
         cy.get('.bucataFiltru.col-sm-12 > #functiiSpecialeSelect > :nth-child(1) > input').check();
         cy.get('.bucataFiltru.col-sm-12 > #functiiSpecialeSelect > :nth-child(2) > input').check();
-        cy.get('.col-lg-4 > .cautaBtn').click();
+    
 
     })
+
+    it("Add product to chart", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get(':nth-child(2) > :nth-child(1) > .title_zone > .mx-auto > .adaugaInCosButton').click();
+        cy.get('.hover').should.exist;
+
+    })
+
+    it("Add product to chart then delete it", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get(':nth-child(2) > :nth-child(1) > .title_zone > .mx-auto > .adaugaInCosButton').click();
+        cy.get('.elemprod').click();
+        cy.get('.hover').should('not.exist');
+
+    })
+
+    it("Add product to chart and order it", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get(':nth-child(2) > :nth-child(1) > .title_zone > .mx-auto > .adaugaInCosButton').click();
+        cy.wait(10000)
+        cy.get(':nth-child(2) > :nth-child(1) > .form-control').type("Stan Marian");
+        cy.get(':nth-child(4) > :nth-child(2) > :nth-child(2) > .form-control').type("0729248288");
+        cy.get(':nth-child(2) > :nth-child(3) > .form-control').type("red.somedayz@gmail.com");
+        cy.get('.dropdown > .btn').click();
+        cy.get(':nth-child(4) > .dropdown-item').click();
+        cy.get('#oraseContainer > .dropdown > .btn').click();
+        cy.get(':nth-child(399) > .dropdown-item').click();
+        cy.get(':nth-child(3) > :nth-child(3) > .form-control').type("str principala nr 375");
+        cy.get('.col-lg-12 > .form-control').type("Vreau sa aiba dot 2023, multumesc.");
+        cy.get('#contactare_telefonica').check();
+        
+    })
+
+
+    it("Visit product specifications", () => {
+        cy.visit("https://www.e-roti.ro/");
+        cy.get('.col-12.row > :nth-child(2) > :nth-child(2)').click()
+    })
+
 } )
 
